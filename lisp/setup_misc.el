710c561065e27b8dcba1afe719059e07a8abc011
Nghia Lam <nghialam12795@gmail.com>
2020-02-18 23:51:27 +0700

    - Setup a more efficient org-mode (Still bug remain)

--- lisp/setup_misc.el
+++ lisp/setup_misc.el
@@ -111,6 +111,167 @@ Use this for files that change often, like cache files."
       (message "Update finished.")))
 )
 
+;; Helper for org-mode
+(defun penguin/find-project-task ()
+  "Move point to the parent (project) task if any."
+  (save-restriction
+    (widen)
+    (let ((parent-task))
+      (while (and (not parent-task)
+                  (org-up-heading-safe))
+        (when (penguin/is-project-p)
+          (setq parent-task (point))))
+      parent-task))
+)
+(defun penguin/is-task-stuck ()
+  "Return non-nil if the task at point is stuck."
+  ;; (bh/list-sublevels-for-projects-indented)
+  (save-restriction
+    (widen)
+    (let ((next-headline (save-excursion (or (outline-next-heading) (point-max))))
+          (this-headline (save-excursion (org-back-to-heading 'invisible-ok) (point))))
+       (let* ((subtree-end (save-excursion (org-end-of-subtree t)))
+              (has-next))
+          (save-excursion
+            (forward-line 1)
+            (while (and (not has-next)
+                        (< (point) subtree-end)
+                        (re-search-forward "^\\*+ NEXT\\|DELEGATED " subtree-end t))
+              ;; Only skip if there is a deadline for delegated tasks
+              (unless (and (member (org-get-todo-state) (list "DELEGATED"))
+                           (not (org-element-property :deadline (org-element-at-point))))
+                (setq has-next t))))
+          (not has-next))))
+)
+(defun penguin/is-project-p ()
+  "Any task with a todo keyword subtask."
+  (save-restriction
+    (widen)
+    (let ((has-subtask)
+          (subtree-end (save-excursion (org-end-of-subtree t)))
+          (is-a-task (and (nth 2 (org-heading-components))
+                          (not (member (nth 2 (org-heading-components)) org-done-keywords)))))
+      (save-excursion
+        (forward-line 1)
+        (while (and (not has-subtask)
+                    (< (point) subtree-end)
+                    (re-search-forward "^\*+ " subtree-end t))
+          (let ((keyword (org-get-todo-state)))
+            (when (and keyword
+                       (not (member keyword (append '("LATER") org-done-keywords))))
+              (setq has-subtask t)))))
+      (and is-a-task has-subtask)))
+)
+(defun penguin/should-skip ()
+  "Return non-nil if all the project subtasks are not stuck."
+  (interactive)
+  (let ((next-heading (save-excursion (or (outline-next-heading) (point-max))))
+        (this-headline (org-element-at-point)))
+    (save-restriction
+      (save-excursion
+        (org-narrow-to-subtree)
+        (let* ((headlines (org-element-map (org-element-parse-buffer 'greater-element t)
+                              'headline
+                            #'identity))
+               ;; Filter out element at point
+               (direct-children (seq-filter (lambda (headline)
+                                              (not (equal (org-element-property :begin this-headline)
+                                                          (org-element-property :begin headline))))
+                                            headlines)))
+          (penguin/should-skip-children direct-children)))))
+)
+(defun penguin/should-skip-children (children)
+  "Check if we should skip the CHILDREN."
+  (widen)
+  (if children
+      ;; If there are any children, recursively see if any task in the subtree is stuck
+      (if (seq-some (lambda (child)
+                      (goto-char (org-element-property :begin child))
+                      (not (penguin/should-skip)))
+                    children)
+          ;; If there are any task which should not be skipped, include it and continue.
+          nil
+        ;; Else we don't care about it
+        next-heading)
+    ;; No children, so check if it is a project task
+    (if (penguin/find-project-task)
+        (if (penguin/is-task-stuck)
+            nil
+          next-heading)
+      next-heading))
+)
+(defun penguin-agenda-prefix ()
+  "Define the prefix."
+  (let* ((deadline (org-element-property :deadline (org-element-at-point)))
+         (level (org-element-property :level (org-element-at-point)))
+         (project-level (org-element-property :level (save-excursion
+                                                       (bh/find-project-task t)
+                                                       (org-element-at-point))))
+         (adjusted (- level project-level))
+         (category (org-entry-get (point) "CATEGORY")))
+    (cond ((and deadline
+                (not (bh/is-subproject-p)))
+           (org-timestamp-format deadline "%x"))
+          ((and (bh/is-subproject-p)
+                (penguin/is-project-p))
+           (penguin-agenda-make-prefix adjusted t))
+          ((penguin/is-project-p)
+           (concat category
+                   ": "))
+          (t (penguin-agenda-make-prefix adjusted)))
+  )
+)
+(defun penguin-agenda-make-prefix (level &optional subproject-p)
+  "Make the agenda prefix with LEVEL and the SUBPROJECT-P."
+  (let ((adjusted (+ 11 level)))
+    (concat (make-string adjusted ?\s)
+            (char-to-string (org-bullets-level-char level))
+            " "))
+)
+(defun bh/find-project-task (&optional top)
+  "Move point to the parent (project) TOP task if any."
+  (save-restriction
+    (widen)
+    (let ((this-task (save-excursion (org-back-to-heading 'invisible-ok) (point)))
+          (parent-task))
+      (while (and (or top (not parent-task)) (org-up-heading-safe))
+        (when (bh/is-project-p)
+          (setq parent-task (point))))
+      (if parent-task
+          (progn
+            (goto-char parent-task)
+            parent-task)
+        (goto-char this-task)
+        this-task)))
+)
+(defun bh/is-subproject-p ()
+  "Any task which is a subtask of another project."
+  (let ((is-subproject)
+        (is-a-task (member (nth 2 (org-heading-components)) org-todo-keywords-1)))
+    (save-excursion
+      (while (and (not is-subproject) (org-up-heading-safe))
+        (when (member (nth 2 (org-heading-components)) org-todo-keywords-1)
+          (setq is-subproject t))))
+    (and is-a-task is-subproject))
+)
+(defun bh/is-project-p ()
+  "Any task with a todo keyword subtask."
+  (save-restriction
+    (widen)
+    (let ((has-subtask)
+          (subtree-end (save-excursion (org-end-of-subtree t)))
+          (is-a-task (not (member (nth 2 (org-heading-components)) org-done-keywords))))
+      (save-excursion
+        (forward-line 1)
+        (while (and (not has-subtask)
+                    (< (point) subtree-end)
+                    (re-search-forward "^\*+ " subtree-end t))
+          (let ((keyword (org-get-todo-state)))
+            (when (and keyword
+                       (not (member keyword (append '("LATER") org-done-keywords))))
+              (setq has-subtask t)))))
+      (and is-a-task has-subtask)))
+)
 
 (provide 'setup_misc)
 ;;; setup_misc.el ends here
--- lisp/setup_org.el
+++ lisp/setup_org.el
@@ -11,46 +11,316 @@
 ;; -----------------------------------------------------------
 
 ;;; Code:
+(require 'setup_misc)
 (require 'setup_hydra)
 
 ;; Setup `Org'
 (use-package org
-  :ensure nil
+  :ensure t
+  :delight "Θ "
+  :bind ("C-c i" . org-insert-structure-template)
   :preface
-  (defun penguin/org-src-buffer-name (org-buffer-name language)
-    "Construct the buffer name for a source editing buffer. See
-`org-src--construct-edit-buffer-name'."
-    (format "*%s*" org-buffer-name)
+  (defun penguin/org-compare-times (clocked estimated)
+    "Gets the ratio between the timed time and the estimated time."
+    (if (and (> (length clocked) 0) estimated)
+        (format "%.2f"
+                (/ (* 1.0 (org-hh:mm-string-to-minutes clocked))
+                   (org-hh:mm-string-to-minutes estimated)))
+      "")
   )
-  (defun penguin/org-set-ongoing-hydra-body ()
-    (setq penguin/ongoing-hydra-body #'hydra-org/body)
+  (defun penguin/org-archive-done-tasks ()
+    "Archives finished or cancelled tasks."
+    (interactive)
+    (org-map-entries
+     (lambda ()
+       (org-archive-subtree)
+       (setq org-map-continue-from (outline-previous-heading)))
+     "TODO=\"DONE\"|TODO=\"CANCELLED\"" (if (org-before-first-heading-p) 'file 'tree))
   )
-  :bind
-  (:map org-mode-map
-        ([remap backward-paragraph] . penguin/backward-paragraph-dwim)
-        ([remap forward-paragraph] . penguin/forward-paragraph-dwim)
-        ("<C-return>" . nil)
-        ("<C-S-down>" . nil)
-        ("<C-S-up>" . nil)
-        ("<M-S-down>" . nil)
-        ("<M-S-up>" . nil))
-  :hook
-  ((org-mode . penguin/org-set-ongoing-hydra-body)
-   (org-mode . org-sticky-header-mode)
-   (org-mode . toc-org-enable))
+  (defun penguin/org-jump ()
+    "Jumps to a specific task."
+    (interactive)
+    (let ((current-prefix-arg '(4)))
+      (call-interactively 'org-refile))
+  )
+  (defun penguin/org-use-speed-commands-for-headings-and-lists ()
+    "Activates speed commands on list items too."
+    (or (and (looking-at org-outline-regexp) (looking-back "^\**"))
+        (save-excursion (and (looking-at (org-item-re)) (looking-back "^[ \t]*"))))
+  )
+  (defmacro ignore-args (fnc)
+    "Returns function that ignores its arguments and invokes FNC."
+    `(lambda (&rest _rest)
+       (funcall ,fnc))
+  )
+  :hook ((after-save . penguin/config-tangle)
+         (auto-save . org-save-all-org-buffers)
+         (org-mode . org-indent-mode))
   :custom
-  (org-descriptive-links nil)
-  (org-edit-src-content-indentation 0)
-  (org-edit-src-persistent-message nil)
-  (org-fontify-done-headline t)
-  (org-fontify-quote-and-verse-blocks t)
-  (org-src-window-setup 'current-window)
+  ;; (org-archive-location "~/.emacs.d/.private/org/.archives/%s::")
+  (org-blank-before-new-entry '((heading . t)
+                                (plain-list-item . t)))
+  (org-cycle-include-plain-lists 'integrate)
+  (org-expiry-inactive-timestamps t)
+  (org-export-backends '(ascii beamer html icalendar latex man md org texinfo))
+  (org-log-done 'time)
+  (org-log-into-drawer "LOGBOOK")
+  (org-modules '(org-crypt
+                 org-habit
+                 org-info
+                 org-irc
+                 org-mouse
+                 org-protocol
+                 org-tempo))
+  (org-refile-allow-creating-parent-nodes 'confirm)
+  (org-refile-use-cache nil)
+  (org-refile-use-outline-path nil)
+  (org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
   (org-startup-folded nil)
-  (org-startup-truncated nil)
-  (org-support-shift-select 'always)
+  (org-startup-with-inline-images t)
+  (org-tag-alist '(("@coding" . ?c)
+                   ("@computer" . ?l)
+                   ("@home" . ?h)
+                   ("@phone" . ?p)
+                   ("@reading" . ?r)
+                   ("@work" . ?b)
+                   ("@writing" . ?w)
+                   ("crypt" . ?C)
+                   ("fuzzy" . ?0)
+                  )
+  )
+  (org-tags-exclude-from-inheritance '("crypt" "project"))
+  (org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+                       (sequence "NOTE(N)" "MEETING(M)")
+                       (sequence "WAITING(w@)" "DELEGATED(e@)" "LATER(l)" "|" "CANCELLED(c)"))
+  )
+
+  (org-use-effective-time t)
+  (org-use-speed-commands 'penguin/org-use-speed-commands-for-headings-and-lists)
+  (org-yank-adjusted-subtrees t)
   :config
-  (advice-add 'org-src--construct-edit-buffer-name :override #'penguin/org-src-buffer-name)
+  (add-to-list 'org-global-properties '("Effort_ALL". "0:05 0:15 0:30 1:00 2:00 3:00 4:00"))
+  (add-to-list 'org-speed-commands-user '("!" penguin/org-clock-in-and-track))
+  (add-to-list 'org-speed-commands-user '("$" call-interactively 'org-archive-subtree))
+  (add-to-list 'org-speed-commands-user '("d" penguin/org-move-line-to-destination))
+  (add-to-list 'org-speed-commands-user '("i" call-interactively 'org-clock-in))
+  (add-to-list 'org-speed-commands-user '("o" call-interactively 'org-clock-out))
+  (add-to-list 'org-speed-commands-user '("s" call-interactively 'org-schedule))
+  (add-to-list 'org-speed-commands-user '("x" org-todo "DONE"))
+  (add-to-list 'org-speed-commands-user '("y" org-todo-yesterday "DONE"))
+  (advice-add 'org-deadline :after (ignore-args #'org-save-all-org-buffers))
+  (advice-add 'org-schedule :after (ignore-args #'org-save-all-org-buffers))
+  (advice-add 'org-store-log-note :after (ignore-args #'org-save-all-org-buffers))
+  (advice-add 'org-todo :after (ignore-args #'org-save-all-org-buffers))
+  (org-clock-persistence-insinuate)
+  (org-load-modules-maybe t)
 )
+(use-package toc-org
+  :after org
+  :hook (org-mode . toc-org-enable)
+)
+(use-package org-indent :ensure nil :after org :delight)
+(use-package org-faces
+  :ensure nil
+  :after org
+  :custom
+  (org-todo-keyword-faces '(("WAITING" :foreground "#fabd2f" :weight bold)
+                            ("DELEGATED" :foreground "#fabd2f" :weight bold)
+                            ("NOTE" :foreground "#83a598" :weight bold)
+                            ("MEETING" :foreground "#83a598" :weight bold)
+                            ("LATER" :foreground "#83a598" :weight bold)
+                            ("NEXT" :foreground "#b8bb26" :weight bold))
+  )
+)
+
+;; Setup `Org-agenda'
+(use-package org-agenda
+  :ensure nil
+  :after org
+  :bind (:map org-agenda-mode-map
+              ("X" . penguin/org-agenda-mark-done-and-add-followup)
+              ("x" . penguin/org-agenda-done))
+  :preface
+  (defun penguin/org-agenda-done (&optional arg)
+    "Mark current TODO as done.
+  This changes the line at point, all other lines in the agenda referring to
+  the same tree node, and the headline of the tree node in the Org-mode file."
+    (interactive "P")
+    (org-agenda-todo "DONE")
+  )
+  (defun penguin/org-agenda-mark-done-and-add-followup ()
+    "Mark the current TODO as done and add another task after it.
+   Creates it at the same level as the previous task, so it's better to use
+   this with to-do items than with projects or headings."
+    (interactive)
+    (org-agenda-todo "DONE")
+    (org-agenda-switch-to)
+    (org-capture 0 "t")
+  )
+  :custom
+  (org-agenda-dim-blocked-tasks t)
+  (org-directory "~/.emacs.d/.private/org/")
+  ;; (org-default-notes-file "~/.personal/agenda/organizer.org")
+  (org-agenda-files (list org-directory))
+  (org-agenda-inhibit-startup t)
+  (org-agenda-show-log t)
+  (org-agenda-skip-deadline-if-done t)
+  (org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
+  (org-agenda-skip-scheduled-if-done t)
+  (org-agenda-span 2)
+  (org-agenda-start-on-weekday 6)
+  (org-agenda-sticky nil)
+  (org-agenda-tags-column -100)
+  (org-agenda-time-grid '((daily today require-timed)))
+  (org-agenda-use-tag-inheritance t)
+  (org-columns-default-format "%14SCHEDULED %Effort{:} %1PRIORITY %TODO %50ITEM %TAGS")
+  (org-enforce-todo-dependencies t)
+  (org-habit-completed-glyph ?✓)
+  (org-habit-graph-column 80)
+  (org-habit-show-habits-only-for-today nil)
+  (org-habit-today-glyph ?‖)
+  (org-track-ordered-property-with-tag t)
+)
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○"))
+  (org-hide-emphasis-markers nil)
+  (org-list-demote-modify-bullet '(("+" . "-") ("1." . "a.") ("-" . "+")))
+  (org-ellipsis "▼")
+  (org-emphasis-alist '(("*" (bold :foreground "Orange" ))
+                        ("/" italic)
+                        ("_" underline)
+                        ("=" (:foreground "maroon"))
+                        ("~" (:foreground "deep sky blue"))
+                        ("+" (:strike-through t)))
+  )
+)
+(use-package org-super-agenda)
+(setq penguin-org-deadline-prefix "%2i%-12(penguin-agenda-prefix)")
+(setq org-agenda-custom-commands '(("n" "Agenda"
+                                    ((agenda "")
+                                     (todo ""
+                                           ((org-agenda-overriding-header (concat (all-the-icons-faicon "chain-broken" :v-adjust 0.01) " Stuck Projects"))
+                                            (org-agenda-skip-function #'penguin/should-skip)
+                                            (org-agenda-prefix-format penguin-org-deadline-prefix)
+                                            (org-agenda-sorting-strategy nil)))
+                                     (todo "NEXT"
+                                           ((org-agenda-overriding-header (concat (all-the-icons-faicon "bolt" :v-adjust 0.01) " Next Tasks"))
+                                            (org-agenda-sorting-strategy
+                                             '(priority-down category-up))))
+                                     (todo "TODO"
+                                           ((org-agenda-files '("~/.emacs.d/.private/org/todo.org" "~/.emacs.d/.private/org/notes.org"))
+                                            (org-agenda-sorting-strategy
+                                             '(priority-down category-up))
+                                            (org-agenda-overriding-header (concat (all-the-icons-faicon "check-square-o" :v-adjust 0.01) " Tasks"))))
+                                     (todo "WAITING|DELEGATED"
+                                           ((org-agenda-overriding-header (concat (all-the-icons-faicon "hourglass" :v-adjust 0.01) " Waiting/Delegated"))
+                                            (org-agenda-sorting-strategy '(priority-down category-up))))
+                                     (todo "LATER"
+                                           ((org-agenda-sorting-strategy
+                                             '(priority-down category-up))
+                                            (org-agenda-overriding-header (concat (all-the-icons-faicon "thumb-tack" :v-adjust 0.01) " Later"))))
+                                     (todo "NOTE"
+                                           ((org-agenda-overriding-header (concat (all-the-icons-faicon "sticky-note" :v-adjust 0.01) " Notes"))
+                                            (org-agenda-max-entries 10)
+                                            (org-agenda-sorting-strategy
+                                             '(tsia-down)))))
+
+                                    nil
+                                   )
+                                  )
+)
+
+
+;; Setup `Org-capture'
+(use-package org-capture
+  :ensure nil
+  :after org
+  :custom
+  (+org-capture-todo-file "~/.emacs.d/.private/org/todo.org")
+  (+org-capture-notes-file "~/.emacs.d/.private/org/notes.org")
+  (org-capture-templates '(("t" "Todo" entry
+                            (file+headline +org-capture-todo-file "Inbox")
+                            "* TODO %?" :prepend t :kill-buffer t)
+                           ("n" "Next" entry
+                            (file+headline +org-capture-todo-file "Inbox")
+                            "* NEXT %?" :prepend t :kill-buffer t)
+                           ("w" "Waiting" entry
+                            (file+headline +org-capture-todo-file "Inbox")
+                            "* WAITING %?" :prepend t :kill-buffer t)
+                           ("o" "Email Note" entry
+                            (file+headline +org-capture-notes-file "Inbox")
+                            "* NOTE %u %^{Content?} :email:\n%a" :prepend t :kill-buffer t)
+                           ("e" "Email" entry
+                            (file+headline +org-capture-todo-file "Inbox")
+                            "* %^{Type?|TODO|NEXT|WAITING|DELEGATED} %^{Content?} :email:\nSCHEDULED: %t\n%a" :prepend t :kill-buffer t)
+                           ("m" "Meeting Notes" entry
+                            (file+headline +org-capture-notes-file "Inbox")
+                            "* NOTE %u %? :meeting:\n** Present at meeting\n- [ ] \n** Agenda\n** Notes" :prepend t :kill-buffer t)
+                           ("N" "Notes" entry
+                            (file+headline +org-capture-notes-file "Inbox")
+                            "* NOTE %u %?\n%i" :prepend t :kill-buffer t)
+                          )
+  )
+)
+
+;; Setup `Org-clock'
+(use-package org-clock
+  :ensure nil
+  :after org
+  :preface
+  (defun penguin/org-mode-ask-effort ()
+    "Ask for an effort estimate when clocking in."
+    (unless (org-entry-get (point) "Effort")
+      (let ((effort
+             (completing-read
+              "Effort: "
+              (org-entry-get-multivalued-property (point) "Effort"))))
+        (unless (equal effort "")
+          (org-set-property "Effort" effort)))))
+  :hook (org-clock-in-prepare-hook . penguin/org-mode-ask-effort)
+  :custom
+  (org-clock-clocktable-default-properties
+   '(:block day :maxlevel 2 :scope agenda :link t :compact t :formula %
+            :step day :fileskip0 t :stepskip0 t :narrow 80
+            :properties ("Effort" "CLOCKSUM" "CLOCKSUM_T" "TODO")))
+  (org-clock-continuously nil)
+  (org-clock-in-switch-to-state "STARTED")
+  (org-clock-out-remove-zero-time-clocks t)
+  (org-clock-persist t)
+  (org-clock-persist-file (expand-file-name (format "%s/emacs/org-clock-save.el" xdg-cache)))
+  (org-clock-persist-query-resume nil)
+  (org-clock-report-include-clocking-task t)
+  (org-show-notification-handler (lambda (msg) (alert msg)))
+)
+
+;; ;; Setup `Org-journal'
+;; (use-package org-journal
+;;   :after org
+;;   :bind (("C-c T" . org-journal-new-entry)
+;;          ("C-c Y" . journal-file-yesterday))
+;;   :preface
+;;   (defun get-journal-file-yesterday ()
+;;     "Gets filename for yesterday's journal entry."
+;;     (let* ((yesterday (time-subtract (current-time) (days-to-time 1)))
+;;            (daily-name (format-time-string "%Y%m%d" yesterday)))
+;;       (expand-file-name (concat org-journal-dir daily-name)))
+;;   )
+;;   (defun journal-file-yesterday ()
+;;     "Creates and load a file based on yesterday's date."
+;;     (interactive)
+;;     (find-file (get-journal-file-yesterday))
+;;   )
+;;   :custom
+;;   (org-journal-date-format "%e %b %Y (%A)")
+;;   (org-journal-dir "~/.emacs.d/.private/org/journal/")
+;;   (org-journal-file-type 'weekly)
+;;   (org-journal-enable-encryption t)
+;;   (org-journal-file-format "%Y%m%d")
+;;   (org-journal-time-format "")
+;; )
+
 
 (provide 'setup_org)
 ;;; setup_org.el ends here
