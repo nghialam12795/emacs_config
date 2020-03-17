1f8cbf3f1a01e0b3bf15e34ec2f3761f1d4236b1
Nghia Lam <nghialam@glassegg.com>
2020-03-17 17:42:25 +0700

    - Optimize Garbage Collection

--- init.el
+++ init.el
@@ -28,25 +28,15 @@
 ;; A big contributor to startup times is garbage collection. We up the gc
 ;; threshold to temporarily prevent it from running, then reset it later by
 ;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
-(setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
 
 ;; In noninteractive sessions, prioritize non-byte-compiled source files to
 ;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
 ;; to skip the mtime checks on every *.elc file.
 (setq load-prefer-newer noninteractive)
 
-;; Make startup faster by reducing the frequency of garbage
-;; collection.
-(setq gc-cons-threshold (* 50 1000 1000))
-
-;; Package archives
-(setq package-user-dir "~/.emacs.d/.local/packages")
-(package-initialize)
-(setq package-archives
-      '(("gnu"   . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")))
-
-;; Load path
+;; Load external path
 (defun update-load-path (&rest _)
   "Update `load-path'."
   (push (expand-file-name "external" user-emacs-directory) load-path))
@@ -58,7 +48,5 @@
     (load-file (expand-file-name "config.el" user-emacs-directory))
   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
 
-;; Make gc pauses faster by decreasing the threshold.
-(setq gc-cons-threshold (* 2 1000 1000))
 
 ;;; init.el ends here
