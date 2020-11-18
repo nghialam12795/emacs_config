fd83b18ff9968cfe1bc9f808c2bc36d9279f6722
Nghia Lam <nghialam12795@gmail.com>
2020-11-18 16:00:06 +0700

    Cleanup

--- init.el
+++ init.el
@@ -1,19 +1,33 @@
 ;;; init.el --- init file for emacs
-
-;; ╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ╮
-;; |  _______ _______ ______  ___ ___ _______ _______ _______  |
-;; | |   _   |   _   |   _  \|   Y   |   _   |   _   |   _   | |
-;; | |.  1   |.  1___|.  |   |.      |.  1   |.  1___|   1___| |
-;; | |.  ____|.  __)_|.  |   |. \_/  |.  _   |.  |___|____   | |
-;; | |:  |   |:  1   |:  |   |:  |   |:  |   |:  1   |:  1   | |
-;; | |::.|   |::.. . |::.|   |::.|:. |::.|:. |::.. . |::.. . | |
-;; | '---'   `-------`--- ---`--- ---`--- ---'-------'-------' |
-;; ╰ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ╯
-
+;; ****************************************************************************
 ;; Author: Nghia Lam
 ;;
 ;; This file is developed based on the author coding habit.
 ;; This may not be suitable for some people.
+
+;; License: The Unlicense
+
+;; This is free and unencumbered software released into the public domain.
+;; Anyone is free to copy, modify, publish, use, compile, sell, or
+;; distribute this software, either in source code form or as a compiled
+;; binary, for any purpose, commercial or non-commercial, and by any
+;; means.
+;; In jurisdictions that recognize copyright laws, the author or authors
+;; of this software dedicate any and all copyright interest in the
+;; software to the public domain. We make this dedication for the benefit
+;; of the public at large and to the detriment of our heirs and
+;; successors. We intend this dedication to be an overt act of
+;; relinquishment in perpetuity of all present and future rights to this
+;; software under copyright law.
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;; IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+;; OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;; OTHER DEALINGS IN THE SOFTWARE.
+;; For more information, please refer to <http://unlicense.org>
+;; ****************************************************************************
 
 ;;; Commentary:
 ;;
@@ -35,13 +49,6 @@
 ;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
 ;; to skip the mtime checks on every *.elc file.
 (setq load-prefer-newer noninteractive)
-
-;; Load external path
-(defun update-load-path (&rest _)
-  "Update `load-path'."
-  (push (expand-file-name "external" user-emacs-directory) load-path))
-
-(update-load-path)
 
 ;; Load config file
 (if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
